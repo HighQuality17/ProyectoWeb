f43b4c70cc524836b3f686bbf0d0151d139e3e9a
jocuartasp <jocuartasp@unal.edu.co>
2024-06-24 16:26:30 -0500

    Se crea el controlador de productos y su migracion, tambien se crea el modelo y migracion de invoice o factura

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -15,16 +15,22 @@ class ProductController extends Controller
 
     public function create()
     {
-        //
+        return view('products.create');
     }
 
     public function store(Request $request)
     {
         $product = new Product();
-        $product->name = $request->product_name;
+        $product->line = $request->product_line;
+        $product->description = $request->product_description;
         $product->price = $request->product_price;
-
+        $product->weight = $request->product_weight;
         $product->stock = $request->product_stock;
+        $product->details = $request->product_details;
+        $product->guarantee = $request->product_guarantee;
+        $product->brand = $request->product_brand;
+        $product->size = $request->product_size;
+        $product->color = $request->product_color;
         $product->save();
         return redirect()->route('products.index');
     }
@@ -36,16 +42,34 @@ class ProductController extends Controller
 
     public function edit(string $id)
     {
-        //
+        //Se utiliza para consultar los datos a editar
+        $product = Product::find($id);
+        return view('products.edit')->with('product', $product);
     }
 
     public function update(Request $request, string $id)
     {
-        //
+        //Se hace el update en la base de datos
+        $product = Product::find($id);
+        $product->line = $request->product_line;
+        $product->description = $request->product_description;
+        $product->price = $request->product_price;
+        $product->weight = $request->product_weight;
+        $product->stock = $request->product_stock;
+        $product->details = $request->product_details;
+        $product->guarantee = $request->product_guarantee;
+        $product->brand = $request->product_brand;
+        $product->size = $request->product_size;
+        $product->color = $request->product_color;
+        $product->save();
+        return redirect()->route('products.index')->with('success', 'Producto creado correctamente.');
     }
 
     public function destroy(string $id)
     {
-        //
+        //Para eliminar un registro en la base de datos
+        $product = Product::find($id);
+        $product->delete();
+        return redirect()->route('products.index');
     }
 }
--- database/migrations/2024_04_11_231158_create_products_table.php
+++ database/migrations/2024_04_11_231158_create_products_table.php
@@ -13,6 +13,16 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('line');
+            $table->string('description');
+            $table->string('price');
+            $table->string('weight');
+            $table->string('stock');
+            $table->string('details');
+            $table->string('guarantee');
+            $table->string('brand');
+            $table->string('size');
+            $table->string('color');
             $table->timestamps();
         });
     }
