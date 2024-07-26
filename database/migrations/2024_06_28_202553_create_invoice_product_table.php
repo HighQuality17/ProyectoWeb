289c8cd1a0fc178e3ef9062875156d9ea64e92f6
jocuartasp <jocuartasp@unal.edu.co>
2024-07-25 20:17:19 -0500

    Se corrigen errores en las llaves foraneas de la tabla invoice_product

--- database/migrations/2024_06_28_202553_create_invoice_product_table.php
+++ database/migrations/2024_06_28_202553_create_invoice_product_table.php
@@ -10,19 +10,20 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-    {
-        Schema::create('invoice_product', function (Blueprint $table) {
-            $table->id();
-            $table->unsignedBigInteger('invoice_id');
-            $table->unsignedBigInteger('product_id');
-            $table->string('quantity');
-            $table->string('subtotal');
-            $table->timestamps();
+{
+    Schema::create('invoice_product', function (Blueprint $table) {
+        $table->id();
+        $table->unsignedBigInteger('invoice_id');
+        $table->unsignedBigInteger('product_id');
+        $table->string('quantity');
+        $table->string('subtotal');
+        $table->timestamps();
 
-            $table->foreignId('invoice_id')->nullable()->constrained('invoices')->cascadeOnUpdate()->nullonDelete();
-            $table->foreignId('product_id')->nullable()->constrained('products  ')->cascadeOnUpdate()->nullonDelete();
-        });
-    }
+        // Foreign keys
+        $table->foreign('invoice_id')->references('id')->on('invoices')->onUpdate('cascade')->onDelete('cascade');
+        $table->foreign('product_id')->references('id')->on('products')->onUpdate('cascade')->onDelete('cascade');
+    });
+}
 
     /**
      * Reverse the migrations.
