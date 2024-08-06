7686d279ca9ea0b07d5bf11b0cc63b66774d84d3
jocuartasp <jocuartasp@unal.edu.co>
2024-08-05 20:20:25 -0500

    Se agregan campos para informacion de la direccion de los clientes en el formulario de registro y se implementa el metodo store en el controlador de direcciones NOTA: aun no funciona el metodo store

--- app/Http/Controllers/AddressController.php
+++ app/Http/Controllers/AddressController.php
@@ -27,7 +27,13 @@ class AddressController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $address = new Address();
+        $address->department = $request->department;
+        $address->city = $request->city;
+        $address->neighborhood = $request->neighborhood;
+        $address->address_line1 = $request->address_line1;
+        $address->address_line2 = $request->address_line2;
+        $address->save();
     }
 
     /**
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -87,14 +87,90 @@
                             </div>
                         </div>
 
+                        <br>
+
+                        <div class="row mb-3">
+                            <label class="col-md-10 col-form-label text-md-end">Ahora necesitamos tu dirección exacta para poder enviar todos tus pedidos!:</label>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="inputDepartment" class="col-md-4 col-form-label text-md-end">{{ __('Departamento:') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="inputDepartment" type="text" class="form-control @error('department') is-invalid @enderror" name="department" value="{{ old('department') }}" required autocomplete="department" autofocus>
+
+                                @error('department')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="inputCity" class="col-md-4 col-form-label text-md-end">{{ __('Ciudad:') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="inputCity" type="text" class="form-control @error('city') is-invalid @enderror" name="city" value="{{ old('city') }}" required autocomplete="city" autofocus>
+
+                                @error('city')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="inputNeighborhood" class="col-md-4 col-form-label text-md-end">{{ __('Barrio:') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="inputNeighborhood" type="text" class="form-control @error('neighborhood') is-invalid @enderror" name="neighborhood" value="{{ old('neighborhood') }}" required autocomplete="neighborhood" autofocus>
+
+                                @error('neighborhood')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="inputAddress1" class="col-md-4 col-form-label text-md-end">{{ __('Dirección 1:') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="inputAddress1" type="text" class="form-control @error('address') is-invalid @enderror" name="address_line1" value="{{ old('address_line1') }}" required autocomplete="address_line1" autofocus>
+
+                                @error('address_line1')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="inputAddress2" class="col-md-4 col-form-label text-md-end">{{ __('Dirección 2:') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="inputAddress2" type="text" class="form-control @error('address_line2') is-invalid @enderror" name="address_line2" value="{{ old('address_line2') }}" required autocomplete="address_line2" autofocus placeholder="Opcional">
+
+                                @error('address_line2')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
                         <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
+                            <div class="col-md-6 offset-md-6">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Registrate!') }}
                                 </button>
                             </div>
                         </div>
-                        <p class="col-md-8 offset-md-4">¿Ya tienes una cuenta? <a href="{{ route('login') }}">Inicia Sesión</a></p>
+                        <p class="col-md-8 offset-md-5">¿Ya tienes una cuenta? <a href="{{ route('login') }}">Inicia Sesión</a></p>
                     </form>
                 </div>
             </div>
