9bebd47eb00f05cd5f2b6807cb976e731a0b3121
vrendonf <vrendonf@unal.edu.co>
2024-09-16 18:27:14 -0500

    corrección en la migracion users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('role_id')->default(2);
-            $table->foreign('role_id')->references('id')->on('roles')->onDelete('set null');
+            $table->foreign('role_id')->references('id')->on('roles');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
@@ -36,3 +36,4 @@ return new class extends Migration
         });
     }
 };
+
